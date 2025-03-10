c20c76b8b364e0ec95c319d69d052cfb768b66ec
deepikaprasadr <deepikaprasad.tsm@gmail.com>
2025-03-09 22:46:47 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using AppInsightErrHndlng.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AppInsightErrHndlng.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            int p = 9;int q = 0; int r = p/q;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
